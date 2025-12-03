3f12f69db7db9c389b452a43115e90b02c1777f7
Salvador <sajuarez1g@gmail.com>
2025-12-02 18:24:03 -0600

    Handlers Get

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,16 +1,35 @@
 import {Request,Response} from 'express'
 import { check,validationResult } from 'express-validator'
-import Products from '../models/Product.model'
+import Product from '../models/Product.model'
+
+export const getProducts = async(req: Request, res: Response) =>{
+    try {
+        const products = await Product.findAll({
+            order:[
+                ["id","ASC"]
+            ]
+        })
+        res.json("pan")
+        res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getProductById = async(req: Request, res: Response) =>{
+    try {
+        console.log(req.params.id)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
 export const createProduct = async (req: Request,res: Response)=>{
     try{
-        const product = await Products.create(req.body)    
+        const product = await Product.create(req.body)    
         res.json({data: product})
     }catch(error){
         console.log(error)
     }
-
-
-
-
 }
--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,23 @@
 import {Router} from 'express'
-import {createProduct} from './handlers/product'
+import {createProduct, getProducts, getProductById} from './handlers/product'
 import {body} from 'express-validator'
 import { handleImputError } from './middleware'
 
 const router = Router()
 
 //Routing
-router.get('/',(req,res)=>{
-    res.json("HOLA get")
-})
+router.get('/', getProducts)
+router.get('/:id', getProductById)
+
 
 router.post('/',
         //validacion
-    body ('name').
-        notEmpty().withMessage('Nombre del producto vacio'),
-    body ('price').isNumeric().withMessage('No es numero').
-        notEmpty().withMessage('Precio del producto vacio').
+    body ('name').notEmpty().withMessage('Nombre del producto vacio'),
+    body ('price').isNumeric().withMessage('No es numero'). notEmpty().withMessage('Precio del producto vacio').
         custom(value => value=>0).withMessage('Precio no valido'),
     handleImputError,
     createProduct)
+
 
 router.put('/',(req,res)=>{
     res.json("HOLA put")
