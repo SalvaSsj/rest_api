fe11deac987a6649de964d599372ab87599ed32f
Salvador <sajuarez1g@gmail.com>
2025-12-04 11:45:06 -0600

    Update y get mediante id

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,7 @@
 import {Request,Response} from 'express'
 import { check,validationResult } from 'express-validator'
 import Product from '../models/Product.model'
+import { json } from 'sequelize'
 
 export const getProducts = async(req: Request, res: Response) =>{
     try {
@@ -9,7 +10,6 @@ export const getProducts = async(req: Request, res: Response) =>{
                 ["id","ASC"]
             ]
         })
-        res.json("pan")
         res.json({data: products})
     } catch (error) {
         console.log(error)
@@ -18,7 +18,19 @@ export const getProducts = async(req: Request, res: Response) =>{
 
 export const getProductById = async(req: Request, res: Response) =>{
     try {
-        console.log(req.params.id)
+        const {id}= req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+
+        await product.update(req.body)
+        await product.save()
+
+        res.json({data: product})
     } catch (error) {
         console.log(error)
     }
@@ -30,6 +42,26 @@ export const createProduct = async (req: Request,res: Response)=>{
         const product = await Product.create(req.body)    
         res.json({data: product})
     }catch(error){
+        console.log(error)
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response)=>{
+try {
+        const {id}= req.params
+        const product = await Product.findByPk(id)
+
+        if(!product){
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+        // Actualizar
+
+
+
+        res.json({data: product})
+    } catch (error) {
         console.log(error)
     }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,27 +1,37 @@
 import {Router} from 'express'
-import {createProduct, getProducts, getProductById} from './handlers/product'
-import {body} from 'express-validator'
+import {createProduct, getProducts, getProductById, updateProduct} from './handlers/product'
+import {body, param} from 'express-validator'
 import { handleImputError } from './middleware'
 
 const router = Router()
 
 //Routing
 router.get('/', getProducts)
-router.get('/:id', getProductById)
 
+router.get('/:id',
+    param('id').isInt().withMessage('Id no valido'), 
+    handleImputError,    
+    getProductById)
 
 router.post('/',
-        //validacion
+    //validacion
     body ('name').notEmpty().withMessage('Nombre del producto vacio'),
-    body ('price').isNumeric().withMessage('No es numero'). notEmpty().withMessage('Precio del producto vacio').
-        custom(value => value=>0).withMessage('Precio no valido'),
+    body ('price').isNumeric().withMessage('No es numero').
+        notEmpty().withMessage('Precio del producto vacio').
+        custom(value => value => 0).withMessage('Precio no valido'),
     handleImputError,
     createProduct)
 
 
-router.put('/',(req,res)=>{
-    res.json("HOLA put")
-})
+router.put('/:id',
+    //validacion
+    body ('name').notEmpty().withMessage('Nombre del producto vacio'),
+    body ('price').isNumeric().withMessage('No es numero').
+        notEmpty().withMessage('Precio del producto vacio').
+        custom(value => value > 0).withMessage('Precio no valido'),
+    body('availability').isBoolean().withMessage('Valor debe ser boolean'),
+    handleImputError,
+    updateProduct)
 
 router.patch('/',(req,res)=>{
     res.json("HOLA patch")
